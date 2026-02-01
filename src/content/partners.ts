cf9c615b4c4e14a8ab4107575ac894fab50c579c
karthik18mohan <karthik18mohan@gmail.com>
2026-02-01 22:17:48 +0530

    Remove placeholder partner images

---
+++ src/content/partners.ts
@@ -0,0 +1,28 @@
+export type PartnerProfile = {
+  name: string;
+  role: string;
+  image: string;
+};
+
+export const partners: PartnerProfile[] = [
+  {
+    name: "Rahul Nathan",
+    role: "Managing Partner",
+    image: "/images/partners/1.jpg"
+  },
+  {
+    name: "Meera Iyer",
+    role: "Audit Partner",
+    image: "/images/partners/2.jpg"
+  },
+  {
+    name: "Vikram Desai",
+    role: "Tax Partner",
+    image: "/images/partners/3.jpg"
+  },
+  {
+    name: "Ananya Rao",
+    role: "Advisory Partner",
+    image: "/images/partners/4.jpg"
+  }
+];
